af43db25452764930da0c09d3a6b6b5ea0e52656
CarlosHeKj <carloslouise2015@gmail.com>
2024-06-24 14:34:19 -0300

    Create Move Count

---
+++ src/chess/pieces/Pawn.java
@@ -0,0 +1,65 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece {
+	public Pawn(Board board,Color color) {
+		super(board,color);
+	}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
+
+	@Override
+	public boolean[][] PossibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position p = new Position(0,0);
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			
+		}
+		//above
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+			
+		}
+		//left
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getColumn() - 1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+		}
+		//bellow
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+		}
+		//right
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getColumn() + 1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+		}
+		return mat;
+	}
+}
